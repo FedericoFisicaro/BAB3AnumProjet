865859fa1634275f479a327db1536f917be5ab4a
federico <171538@umons.ac.be>
2019-05-27 23:00:10 +0200

    test

---
+++ projet.m
@@ -0,0 +1,13 @@
+tic %pour lancer le chronometre
+close all
+clear all
+
+global zMin,zMax, D, mu, beta1, beta2; %données du probléme
+global n
+
+zMin=0;
+zMax=50;
+D=0,01;
+mu=0,08;
+beta1= 0,008;
+beta2=2,54*beta1;
